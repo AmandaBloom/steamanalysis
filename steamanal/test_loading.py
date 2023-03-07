d0171908305c71f75c13831749cc979b6102426d
TomaszPawlak <01149768@pw.edu.pl>
2023-03-07 14:17:33 +0100

    full data

--- steamanal/test_loading.py
+++ steamanal/test_loading.py
@@ -1,16 +1,19 @@
 from steammarket import get_multiple, get_csgo_item
 from decimal import Decimal, getcontext
 import json
-getcontext().prec = 10 # Setting 10 digits precision for Decimal numbers
+getcontext().prec = 10  # Setting 10 digits precision for Decimal numbers
 
 with open("src/item_data.json", "r") as file:
+    
     data = json.load(file)
+    print(data)
     for item in data:
         requested_item = get_csgo_item(item)
-        price_now = Decimal(requested_item["lowest_price"][:-2].replace(',', '.'))
 
-        print(price_now)
-        earnded = price_now - Decimal(data[item]["buying_price"]).normalize()
-        print("Earned", earnded * data[item]["quantity"])
-        print(requested_item)
+        if requested_item is not None:
+            price_now = Decimal(requested_item["lowest_price"][:-2].replace(',', '.'))
 
+            print(price_now)
+            earnded = price_now - Decimal(data[item]["buying_price"]).normalize()
+            print("Earned", earnded * data[item]["quantity"])
+        print(item, requested_item, '\n')
