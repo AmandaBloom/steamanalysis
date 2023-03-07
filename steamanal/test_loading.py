81dd2d43242beef1bb308a1d479d75b1fbc98bfb
Paweł Skorupski <pawel.skor14@outlook.com>
2023-03-07 11:41:29 +0100

    Example data scraping with decimal precision

--- steamanal/test_loading.py
+++ steamanal/test_loading.py
@@ -1,6 +1,16 @@
 from steammarket import get_multiple, get_csgo_item
+from decimal import Decimal, getcontext
+import json
+getcontext().prec = 10 # Setting 10 digits precision for Decimal numbers
 
-with open("src/item_names.txt", "r") as file:
-    for name in file:
-        print(get_csgo_item(name))
+with open("src/item_data.json", "r") as file:
+    data = json.load(file)
+    for item in data:
+        requested_item = get_csgo_item(item)
+        price_now = Decimal(requested_item["lowest_price"][:-2].replace(',', '.'))
+
+        print(price_now)
+        earnded = price_now - Decimal(data[item]["buying_price"]).normalize()
+        print("Earned", earnded * data[item]["quantity"])
+        print(requested_item)
 
