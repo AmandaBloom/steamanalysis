16950608d0a314c6f5f19ff40daf70f52c5819b8
TomaszPawlak <01149768@pw.edu.pl>
2023-03-07 14:19:52 +0100

    test for loading added

---
+++ steamanal/test_load1.py
@@ -0,0 +1,8 @@
+import steammarket as sm
+
+for i in range(2):
+    tmp = sm.get_item(appid=730, name="Sticker | dupreeh | Berlin 2019", currency='PLN')
+    print(i, tmp)
+for i in range(2):
+    tmp = sm.get_item(appid=730, name="Sticker | RpK | Berlin 2019", currency='PLN')
+    print(i, tmp)
